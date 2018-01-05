47aa348229a2831cf64ed9f1dfec0cc3cd4dd4d2
Lorenzo Benvenuti <lorenzo.benvenuti@gmail.com>
2018-01-05 08:30:18 +0100

    New project layout

--- cmd/hutemon/main.go
+++ cmd/hutemon/main.go
@@ -6,12 +6,13 @@ import (
 	"os"
 
 	"github.com/golang/glog"
+	"github.com/lorenzobenvenuti/hutemon/weather"
 )
 
 func main() {
 	flag.Parse()
 	key := os.Args[1]
-	w, err := NewWUndergroundWeatherProvider(key).GetWeather(os.Args[2])
+	w, err := weather.NewWUndergroundWeatherProvider(key).GetWeather(os.Args[2])
 	if err != nil {
 		glog.Fatal(err)
 	}
