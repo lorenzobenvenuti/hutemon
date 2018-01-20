34b186b5171ba25f9facee64b70f69eced803d54
Lorenzo Benvenuti <lorenzo.benvenuti@gmail.com>
2018-01-20 18:29:34 +0100

    Handler chain

--- cmd/hutemon/main.go
+++ cmd/hutemon/main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"os"
+	"time"
 
-	"github.com/golang/glog"
+	"github.com/lorenzobenvenuti/hutemon/handlers"
 	"github.com/lorenzobenvenuti/hutemon/weather"
 )
 
 func main() {
-	flag.Parse()
-	wuKey := os.Args[1]
-	owmKey := os.Args[2]
-	wp := make([]weather.Provider, 2)
-	wp[0] = weather.NewWUndergroundProvider(wuKey)
-	wp[1] = weather.NewOpenWeatherMapProvider(owmKey)
-	w, err := weather.NewProviderChain(wp).GetWeather(os.Args[3])
-	if err != nil {
-		glog.Fatal(err)
-	}
-	fmt.Printf("%#v\n", w)
+	// flag.Parse()
+	// wuKey := os.Args[1]
+	// owmKey := os.Args[2]
+	// wp := make([]weather.Provider, 2)
+	// wp[0] = weather.NewWUndergroundProvider(wuKey)
+	// wp[1] = weather.NewOpenWeatherMapProvider(owmKey)
+	// w, err := weather.NewProviderChain(wp).GetWeather(os.Args[3])
+	// if err != nil {
+	// 	glog.Fatal(err)
+	// }
+	// fmt.Printf("%#v\n", w)
+	w := &weather.Weather{}
+	h := make(map[string]handlers.Handler)
+	h["a"] = handlers.NewDummyHandler("A", 1*time.Second)
+	h["b"] = handlers.NewDummyHandler("B", 2*time.Second)
+	h["c"] = handlers.NewDummyHandler("C", 3*time.Second)
+	err := handlers.NewHandlerChain(h).Handle(w)
+	fmt.Println(err)
 }
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,7 +1,81 @@
 package handlers
 
-import "github.com/lorenzobenvenuti/hutemon/weather"
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/lorenzobenvenuti/hutemon/weather"
+)
 
 type Handler interface {
-	Handle(w weather.Weather) error
+	Handle(w *weather.Weather) error
+}
+
+type dummyHandler struct {
+	name    string
+	timeout time.Duration
+}
+
+func (dh *dummyHandler) Handle(w *weather.Weather) error {
+	time.Sleep(dh.timeout)
+	return fmt.Errorf("Error %s", dh.name)
+}
+
+func NewDummyHandler(name string, timeout time.Duration) Handler {
+	return &dummyHandler{name: name, timeout: timeout}
+}
+
+type handlerError struct {
+	name string
+	err  error
+}
+
+type multiError struct {
+	errors map[string]error
+}
+
+func (me *multiError) Error() string {
+	msgs := make([]string, len(me.errors))
+	for k, e := range me.errors {
+		msgs = append(msgs, fmt.Sprintf("%v -> %s", k, e))
+	}
+	return strings.Join(msgs, "\n")
+}
+
+type handlerChain struct {
+	handers map[string]Handler
+}
+
+func (hc *handlerChain) Handle(w *weather.Weather) error {
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	errors := make(map[string]error)
+	queue := make(chan handlerError, 1)
+	wg.Add(len(hc.handers))
+	for k, h := range hc.handers {
+		go func(name string, handler Handler) {
+			queue <- handlerError{name: name, err: handler.Handle(w)}
+		}(k, h)
+		go func() {
+			for he := range queue {
+				if he.err != nil {
+					mutex.Lock()
+					errors[he.name] = he.err
+					mutex.Unlock()
+				}
+				wg.Done()
+			}
+		}()
+	}
+	wg.Wait()
+	if len(errors) == 0 {
+		return nil
+	}
+	return &multiError{errors: errors}
+}
+
+func NewHandlerChain(handlers map[string]Handler) Handler {
+	return &handlerChain{handers: handlers}
 }
